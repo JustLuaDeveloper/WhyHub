39f7a9db09cc9d9ff27ec74224db9ec21b34ad7b
JustLuaDeveloper <stasmaks5423@gmail.com>
2025-10-27 11:47:27 +0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -3,7 +3,7 @@ local games = {
     ["7436755782"] = reverse("daolnwod/b13bdd81b894553c977958c205ccd6febf90f2c06e7337efa492b3668edc97d47/cilbup/stpircsauL/1v/ipa/ed.tnempoleved-eknij.nipa//:sptth")
 }
 if games[tostring(GameId)] then
-    loadstring(game:HttpGetAsync(games[tostring(GameId)]))()
+    loadstring(game:HttpGet(games[tostring(GameId)]))()
 else
     game.StarterGui:SetCore("SendNotification", {
         Title = "WhyLoader",
