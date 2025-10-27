71ee9781d7f6a155dceac9e7b52711daf26a146d
JustLuaDeveloper <stasmaks5423@gmail.com>
2025-10-27 11:38:37 +0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -3,7 +3,7 @@ local games = {
     ["7436755782"] = "https://api.junkie-development.de/api/v1/luascripts/public/74dc88e366b294afe7e33760c2f09fbef6dcc502c859779c3557498d18bbd31b/download"
 }
 if games[tostring(GameId)] then
-    loadstring(games[tostring(GameId)])()
+    loadstring(game:HttpGetAsync(games[tostring(GameId)]))()
 else
     game.StarterGui:SetCore("SendNotification", {
         Title = "WhyLoader",
