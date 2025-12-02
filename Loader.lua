fa75c1fd68bda9b6dc94b015298bfba21c50b910
JustLuaDeveloper <stasmaks5423@gmail.com>
2025-12-02 10:05:52 +0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,4 +1,6 @@
-getgenv().flag = "DAHJDAMMCAJDAHYYDAH"
+JunkieCore = nil
+JD = nil
+JunkieProtected = nil
 local url = "https://api.junkie-development.de/api/v1/luascripts/public/dddccd0ae232b69eb42d336a7e25eae7713e910232003f3f54e86edb1b938918/download"
 
 local code = game:HttpGet(url, true)
